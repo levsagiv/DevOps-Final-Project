a65cbb611139fafcefc947a86952764e9c092e5c
Sagiv Levy <levsagiv@gmail.com>
2023-01-06 15:35:34 +0200

    add js file

---
+++ index.js
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>My Web Application</title>
+</head>
+<body>
+
+<h1>My Web Application</h1>
+
+<p>Welcome to my web application!</p>
+
+<%
+    String userName = request.getParameter("userName");
+    if (userName != null) {
+        out.println("<p>Hello, " + userName + "!</p>");
+    } else {
+        out.println("<p>Please enter your name:</p>");
+        out.println("<form action='index.jsp'>");
+        out.println("<input type='text' name='userName' />");
+        out.println("<input type='submit' value='Submit' />");
+        out.println("</form>");
+    }
+%>
+
+</body>
+</html>
